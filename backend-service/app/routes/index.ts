761d061542225aea69e3d200086c03fb71785a6f
Thibaut-Frontori <thibaut.frontori@oclock.school>
2025-04-10 23:37:57 +0200

    Revert "Merge pull request #2 from O-clock-Sigyn/thibaut"
    This reverts commit 6e1c11966ddd8ebc97210fe77bd5065d5cd2c4d8, reversing
    changes made to bffcf55b1cc36f0161b34920b59f42d513f3a2fa.

--- backend-service/app/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authController from "../controllers/authController";
-
-const routes = express.Router();
-routes.get("/login", authController.login);
-routes.get("/register", authController.register);
-
-export default routes;
