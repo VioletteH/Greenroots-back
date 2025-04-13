6e4d4eb002f5ac5ffb029605669d2363822179c0
Thibaut-Frontori <thibaut.frontori@oclock.school>
2025-04-13 10:53:42 +0200

    feat: add error handler middleware and refactor login to use AppError

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './app/routes/index';
 import "dotenv/config";
+import { errorHandler } from './app/middlewares/errorHandler';
 
 
 const app = express()
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
